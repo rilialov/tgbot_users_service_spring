5a2418d62716c7b008d29dfc114af360f3b583c7
rilialov <ilialovrustem@gmail.com>
2022-05-28 15:33:32 +0300

    Added getUsers method to endpoint

--- src/main/java/tgbot/users_service/endpoint/UserEndpoint.java
+++ src/main/java/tgbot/users_service/endpoint/UserEndpoint.java
@@ -4,14 +4,15 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import tgbot.users.service.GetUserByIdRequest;
-import tgbot.users.service.GetUserByNickRequest;
-import tgbot.users.service.GetUserResponse;
+import tgbot.users.service.*;
 import tgbot.users_service.converters.UserConverter;
 import tgbot.users_service.entity.User;
 import tgbot.users_service.repository.UserRepository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Endpoint
 public class UserEndpoint {
@@ -19,7 +20,7 @@ public class UserEndpoint {
     private static final String NAMESPACE_URI = "http://users.tgbot/service";
 
     private final UserRepository userRepository;
-    private UserConverter userConverter = new UserConverter();
+    private final UserConverter userConverter = new UserConverter();
 
     public UserEndpoint(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -42,6 +43,20 @@ public class UserEndpoint {
 
         Optional<User> optional = userRepository.findByNickname(request.getNickname());
         optional.ifPresent(user -> response.setUserDTO(userConverter.userToUserDTO(user)));
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getUsersRequest")
+    @ResponsePayload
+    public GetUsersResponse getUsers(@RequestPayload GetUsersRequest request) {
+        GetUsersResponse response = new GetUsersResponse();
+
+        Iterable<User> iterable = userRepository.findAll();
+        List<User> userList =
+                StreamSupport.stream(iterable.spliterator(), false)
+                        .collect(Collectors.toList());
+        List<UserDTO> userDTOList = userConverter.userListToDTOUserList(userList);
+        response.setUsersList(userDTOList);
         return response;
     }
 
